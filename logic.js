d5f0f3918f298ccb71ab74a4284172326379a213
unknown <maggie.ulrich@gmail.com>
2019-03-31 11:44:52 -0400

    homework commit

---
+++ logic.js
@@ -0,0 +1,134 @@
+// Store our API endpoint inside queryUrl
+var earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+var tectonicPlatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+
+// Perform a GET request to the query URL
+d3.json(earthquakeURL, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the magnitude, place and time of the earthquake
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3>Magnitude: " + feature.properties.mag +"</h3><h3>Location: "+ feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    },
+    
+    pointToLayer: function (feature, latlng) {
+      return new L.circle(latlng,
+        {radius: getRadius(feature.properties.mag),
+        fillColor: getColor(feature.properties.mag),
+        fillOpacity: .6,
+        color: "#000",
+        stroke: true,
+        weight: .8
+    })
+  }
+  });
+
+  
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+
+function createMap(earthquakes) {
+
+    // Define different map layers
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?" +
+      "access_token=pk.eyJ1IjoidGJlcnRvbiIsImEiOiJjamRoanlkZXIwenp6MnFuOWVsbGo2cWhtIn0.zX40X0x50dpaN96rKQKarw." +
+      "T6YbdDixkOBWH_k9GbS8JQ");
+
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
+      "access_token=pk.eyJ1IjoidGJlcnRvbiIsImEiOiJjamRoanlkZXIwenp6MnFuOWVsbGo2cWhtIn0.zX40X0x50dpaN96rKQKarw." +
+      "T6YbdDixkOBWH_k9GbS8JQ");
+    
+    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+      "access_token=pk.eyJ1IjoidGJlcnRvbiIsImEiOiJjamRoanlkZXIwenp6MnFuOWVsbGo2cWhtIn0.zX40X0x50dpaN96rKQKarw." +
+      "T6YbdDixkOBWH_k9GbS8JQ");
+  
+  
+    // Define a baseMaps object to hold our base layers
+    // Pass in our baseMaps 
+    var baseMaps = {
+      "Satellite": satellite,
+      "Dark Map": darkmap,
+      "Outdoors": outdoors
+    };
+
+    // Creat a layer for the tectonic plates
+    var tectonicPlates = new L.LayerGroup();
+
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+      "Earthquakes": earthquakes,
+      "Tectonic Plates": tectonicPlates
+    };
+
+    // Create our map, giving it the satellite, earthquakes and tectonic plates layers to display on load
+    var myMap = L.map("map", {
+      center: [
+        37.09, -75.71],
+      zoom: 3.25,
+      layers: [satellite, earthquakes, tectonicPlates]
+    }); 
+
+    // Add Fault lines data
+    d3.json(tectonicPlatesURL, function(plateData) {
+      // Adding our geoJSON data, along with style information, to the tectonicplates
+      // layer.
+      L.geoJson(plateData, {
+        color: "cyan",
+        weight: 0.5
+      })
+      .addTo(tectonicPlates);
+  });
+
+  
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+
+  //Create a legend on the bottom left
+  var legend = L.control({position: 'bottomleft'});
+
+    legend.onAdd = function(myMap){
+      var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4, 5],
+        labels = [];
+
+  // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+
+  legend.addTo(myMap);
+}
+   
+
+  //Create color range for the circle diameter 
+  function getColor(d){
+    return d > 5 ? "#a54500":
+    d  > 4 ? "#cc5500":
+    d > 3 ? "#ff6f08":
+    d > 2 ? "#ff9143":
+    d > 1 ? "#ffb37e":
+             "#ffcca5";
+  }
+
+  //Change the maginutde of the earthquake by a factor of 25,000 for the radius of the circle. 
+  function getRadius(value){
+    return value*25000
+  }
