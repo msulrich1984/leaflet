89f5b103b5180e2b79a83bd2efbb868c4458d899
Margaret Ulrich <maggie.ulrich@gmail.com>
2021-02-26 20:13:49 -0500

    Update logic.js
    Key is updated

--- logic.js
+++ logic.js
@@ -47,7 +47,7 @@ function createMap(earthquakes) {
       "T6YbdDixkOBWH_k9GbS8JQ");
 
     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
-      "access_token=pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xtem11dmwwNjFiMnVxbWJkbndydTJuIn0.-67p4ssJ-6AcglhLo2o6nA" +
+      "access_token=pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xtem11dmwwNjFiMnVxbWJkbndydTJuIn0.-67p4ssJ-6AcglhLo2o6nA." +
       "T6YbdDixkOBWH_k9GbS8JQ");
     
     var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
