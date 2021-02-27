289e4ff09f845932a4f7188546e6cc7409a3d768
Margaret Ulrich <maggie.ulrich@gmail.com>
2021-02-27 10:09:35 -0500

    update

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+const API_KEY = "pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xuMTZna20wZXA0MnpxcGdseWNibHBoIn0.mWGeTNlf8dhTQgwduhMCWw";
--- logic.js
+++ logic.js
@@ -41,28 +41,34 @@ function createFeatures(earthquakeData) {
 
 function createMap(earthquakes) {
 
-    // Define different map layers
-    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?" +
-      "access_token=pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xuMTZna20wZXA0MnpxcGdseWNibHBoIn0.mWGeTNlf8dhTQgwduhMCWw" +
-      "T6YbdDixkOBWH_k9GbS8JQ");
+    // Define streetmap, lightmap and satellite layers
 
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
-      "access_token=pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xuMTZna20wZXA0MnpxcGdseWNibHBoIn0.mWGeTNlf8dhTQgwduhMCWw" +
-      "T6YbdDixkOBWH_k9GbS8JQ");
-    
-    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-      "access_token=pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xuMTZna20wZXA0MnpxcGdseWNibHBoIn0.mWGeTNlf8dhTQgwduhMCWw" +
-      "T6YbdDixkOBWH_k9GbS8JQ");
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.light",
+      accessToken: "pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xuMTZna20wZXA0MnpxcGdseWNibHBoIn0.mWGeTNlf8dhTQgwduhMCWw"
+    });
+    var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.streets",
+      accessToken: "pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xuMTZna20wZXA0MnpxcGdseWNibHBoIn0.mWGeTNlf8dhTQgwduhMCWw"
+    });
+    var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.satellite",
+      accessToken: "pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xuMTZna20wZXA0MnpxcGdseWNibHBoIn0.mWGeTNlf8dhTQgwduhMCWw"
   
+    });
   
     // Define a baseMaps object to hold our base layers
-    // Pass in our baseMaps 
     var baseMaps = {
-      "Satellite": satellite,
-      "Dark Map": darkmap,
-      "Outdoors": outdoors
+      "Light Map": lightmap,
+      "Street Map": streetmap,
+      "Satellite Map" : satellite    
     };
-
     // Creat a layer for the tectonic plates
     var tectonicPlates = new L.LayerGroup();
 
